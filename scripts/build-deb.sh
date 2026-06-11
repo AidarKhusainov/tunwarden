64b4b2d51a20af87eb515ea0a4701a1f87af8672
Aidar Khusainov <khusainov19978@gmail.com>
2026-06-11 15:07:22 +0500

    packaging: fix lintian binary and compression errors

--- scripts/build-deb.sh
+++ scripts/build-deb.sh
@@ -51,16 +51,16 @@ mkdir -p \
   "${root_dir}/usr/share/man/man8" \
   "${root_dir}/usr/share/doc/tunwarden"
 
-GOOS=linux GOARCH="${goarch}" go build -trimpath -o "${root_dir}/usr/bin/tunwarden" ./cmd/tunwarden
-GOOS=linux GOARCH="${goarch}" go build -trimpath -o "${root_dir}/usr/bin/tunwardend" ./cmd/tunwardend
+CGO_ENABLED=0 GOOS=linux GOARCH="${goarch}" go build -trimpath -ldflags "-s -w" -o "${root_dir}/usr/bin/tunwarden" ./cmd/tunwarden
+CGO_ENABLED=0 GOOS=linux GOARCH="${goarch}" go build -trimpath -ldflags "-s -w" -o "${root_dir}/usr/bin/tunwardend" ./cmd/tunwardend
 
 install -m 0644 packaging/systemd/tunwardend.service "${root_dir}/usr/lib/systemd/system/tunwardend.service"
 install -m 0644 packaging/sysusers.d/tunwarden.conf "${root_dir}/usr/lib/sysusers.d/tunwarden.conf"
-gzip -n -c docs/man/tunwarden.1 > "${root_dir}/usr/share/man/man1/tunwarden.1.gz"
-gzip -n -c docs/man/tunwardend.8 > "${root_dir}/usr/share/man/man8/tunwardend.8.gz"
+gzip -9n -c docs/man/tunwarden.1 > "${root_dir}/usr/share/man/man1/tunwarden.1.gz"
+gzip -9n -c docs/man/tunwardend.8 > "${root_dir}/usr/share/man/man8/tunwardend.8.gz"
 install -m 0644 README.md LICENSE "${root_dir}/usr/share/doc/tunwarden/"
 install -m 0644 LICENSE "${root_dir}/usr/share/doc/tunwarden/copyright"
-printf 'tunwarden (%s) unstable; urgency=medium\n\n  * Local development package build.\n\n -- Aidar Khusainov <19706697+AidarKhusainov@users.noreply.github.com>  Thu, 11 Jun 2026 00:00:00 +0000\n' "${version}" | gzip -n -c > "${root_dir}/usr/share/doc/tunwarden/changelog.Debian.gz"
+printf 'tunwarden (%s) unstable; urgency=medium\n\n  * Local development package build.\n\n -- Aidar Khusainov <19706697+AidarKhusainov@users.noreply.github.com>  Thu, 11 Jun 2026 00:00:00 +0000\n' "${version}" | gzip -9n -c > "${root_dir}/usr/share/doc/tunwarden/changelog.gz"
 find docs -type f ! -path 'docs/man/*' -print | while IFS= read -r file; do
   target="${root_dir}/usr/share/doc/tunwarden/${file}"
   mkdir -p "$(dirname "${target}")"
