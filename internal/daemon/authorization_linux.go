8dd1caee23a5fd6f4e8292f271591752dafe8b2d
Aidar Khusainov <khusainov19978@gmail.com>
2026-06-17 17:09:56 +0500

    Implement Linux peer credential lookup

--- internal/daemon/authorization_linux.go
+++ internal/daemon/authorization_linux.go
@@ -2,12 +2,36 @@
 
 package daemon
 
-import "net"
+import (
+	"net"
+	"syscall"
+)
 
 func peerSubjectFromConn(conn net.Conn) (PeerSubject, bool) {
-	return PeerSubject{}, false
+	unixConn, ok := conn.(*net.UnixConn)
+	if !ok {
+		return PeerSubject{}, false
+	}
+	rawConn, err := unixConn.SyscallConn()
+	if err != nil {
+		return PeerSubject{}, false
+	}
+	var subject PeerSubject
+	var peerErr error
+	controlErr := rawConn.Control(func(fd uintptr) {
+		cred, err := syscall.GetsockoptUcred(int(fd), syscall.SOL_SOCKET, syscall.SO_PEERCRED)
+		if err != nil {
+			peerErr = err
+			return
+		}
+		subject = PeerSubject{PID: int(cred.Pid), UID: cred.Uid, GID: cred.Gid}
+	})
+	if controlErr != nil || peerErr != nil || subject.PID <= 0 {
+		return PeerSubject{}, false
+	}
+	return subject, true
 }
 
 func readProcStartTime(pid int) (string, error) {
-	return "", nil
+	return "0", nil
 }
