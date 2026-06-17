df287ee81fa414218523c8db42f864027f250759
Aidar Khusainov <khusainov19978@gmail.com>
2026-06-17 18:05:09 +0500

    Remove temp file

--- internal/daemon/subject_probe.go
+++
@@ -1,7 +0,0 @@
-package daemon
-
-import "strconv"
-
-func subjectProbe(pid int, start uint64, uid uint32) string {
-	return strconv.Itoa(pid) + "," + strconv.FormatUint(start, 10) + "," + strconv.FormatUint(uint64(uid), 10)
-}
