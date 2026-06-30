cb4ee62b549ec58847c80eabd6cbb4eb8425cc56
Aidar Khusainov <khusainov19978@gmail.com>
2026-06-30 15:40:10 +0300

    Add stable daemon API error categories

---
+++ internal/daemon/api_errors.go
@@ -0,0 +1,104 @@
+package daemon
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/AidarKhusainov/podlaz/internal/profile"
+)
+
+type daemonAPIErrorCategory string
+
+const (
+	daemonAPIErrorBadRequest         daemonAPIErrorCategory = "bad_request"
+	daemonAPIErrorConflict           daemonAPIErrorCategory = "conflict"
+	daemonAPIErrorAccessDenied       daemonAPIErrorCategory = "access_denied"
+	daemonAPIErrorServiceUnavailable daemonAPIErrorCategory = "service_unavailable"
+	daemonAPIErrorInternal           daemonAPIErrorCategory = "internal"
+)
+
+type daemonAPIError struct {
+	category daemonAPIErrorCategory
+	err      error
+}
+
+func (e *daemonAPIError) Error() string {
+	if e == nil || e.err == nil {
+		return "daemon API error"
+	}
+	return e.err.Error()
+}
+
+func (e *daemonAPIError) Unwrap() error {
+	if e == nil {
+		return nil
+	}
+	return e.err
+}
+
+func categorizeDaemonAPIError(category daemonAPIErrorCategory, err error) error {
+	if err == nil {
+		return nil
+	}
+	return &daemonAPIError{category: category, err: err}
+}
+
+func daemonAPIBadRequest(err error) error {
+	return categorizeDaemonAPIError(daemonAPIErrorBadRequest, err)
+}
+
+func daemonAPIConflict(err error) error {
+	return categorizeDaemonAPIError(daemonAPIErrorConflict, err)
+}
+
+func daemonAPIAccessDenied(err error) error {
+	return categorizeDaemonAPIError(daemonAPIErrorAccessDenied, err)
+}
+
+func daemonAPIServiceUnavailable(err error) error {
+	return categorizeDaemonAPIError(daemonAPIErrorServiceUnavailable, err)
+}
+
+func daemonAPIInternal(err error) error {
+	return categorizeDaemonAPIError(daemonAPIErrorInternal, err)
+}
+
+func daemonAPIHTTPStatusCode(err error) int {
+	if profile.IsValidationError(err) {
+		return http.StatusBadRequest
+	}
+	var apiErr *daemonAPIError
+	if !errors.As(err, &apiErr) {
+		return http.StatusInternalServerError
+	}
+	switch apiErr.category {
+	case daemonAPIErrorBadRequest:
+		return http.StatusBadRequest
+	case daemonAPIErrorConflict:
+		return http.StatusConflict
+	case daemonAPIErrorAccessDenied:
+		return http.StatusForbidden
+	case daemonAPIErrorServiceUnavailable:
+		return http.StatusServiceUnavailable
+	default:
+		return http.StatusInternalServerError
+	}
+}
+
+func writeDaemonAPIHTTPError(w http.ResponseWriter, err error) {
+	if err == nil {
+		return
+	}
+	http.Error(w, err.Error(), daemonAPIHTTPStatusCode(err))
+}
+
+func categorizeAuthorizationError(err error) error {
+	switch {
+	case errors.Is(err, ErrAuthorizationDenied):
+		return daemonAPIAccessDenied(err)
+	case errors.Is(err, ErrAuthorizationUnavailable):
+		return daemonAPIServiceUnavailable(err)
+	default:
+		return daemonAPIInternal(err)
+	}
+}
